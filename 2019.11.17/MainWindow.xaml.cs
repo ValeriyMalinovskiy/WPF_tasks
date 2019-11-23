995112bf08e956b37cb7bc1e282fd2408ecd3cd4
ValeriyMalinovskiy <47790956+ValeriyMalinovskiy@users.noreply.github.com>
2019-11-23 06:46:47 +0200

    local commit

---
+++ 2019.11.17/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _2019._11._17
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void add_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(category.Text) || string.IsNullOrWhiteSpace(price.Text) || string.IsNullOrWhiteSpace(vendor_code.Text)||string.IsNullOrWhiteSpace(category.Text))
+            {
+            }
+        }
+    }
+}
