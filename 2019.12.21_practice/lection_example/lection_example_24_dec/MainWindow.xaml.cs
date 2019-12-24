d17556561c4dbcf7c73c1c415dc75c524f11ba1a
ValeriyMalinovskiy <47790956+ValeriyMalinovskiy@users.noreply.github.com>
2019-12-24 19:41:49 +0200

    local

---
+++ 2019.12.21_practice/lection_example/lection_example_24_dec/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lection_example_24_dec
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    class Pupil
+    {
+        bool IsManager;
+        public string FirstName { get; private set; }
+        public Pupil(string firstName)
+        {
+            this.FirstName = firstName;
+        }
+    }
+}
